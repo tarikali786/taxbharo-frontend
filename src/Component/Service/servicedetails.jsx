d7ef0ba8309b3853e37e1256fba78052ae614e79
Tariq Ali <tarikali7444@gmail.com>
2025-02-13 19:38:22 +0530

    add glow effect on button

--- src/Component/Service/servicedetails.jsx
+++ src/Component/Service/servicedetails.jsx
@@ -109,7 +109,9 @@ export const ServiceDetails = () => {
               <div className="mt-6 flex gap-10 items-center flex-wrap">
                 <Link
                   to={serviceDetails?.attributes?.Button_Url}
-                  className="bg-blue-500 md:px-6 md:py-2 px-2 py-1  text-nowrap  rounded-md text-white-500"
+                  className="relative bg-blue-500 md:px-6 md:py-2 px-4 py-2 rounded-md text-white-500  text-center transition-transform duration-300 hover:scale-105 
+                 shadow-[0_0_20px_rgba(71,98,255,0.7),0_0_40px_rgba(71,98,255,0.5),0_0_60px_rgba(71,98,255,0.3)] 
+                 hover:shadow-[0_0_25px_rgba(71,98,255,1),0_0_50px_rgba(71,98,255,0.8),0_0_75px_rgba(71,98,255,0.6)]"
                 >
                   {serviceDetails?.attributes?.Button_name
                     ? serviceDetails.attributes.Button_name
